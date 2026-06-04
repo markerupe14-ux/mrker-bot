aa912bf6d32e27dd618bc731bad91ffcc147425d
markerupe14-ux <markerupe14@gmail.com>
2026-06-04 21:05:08 +0300

    Add authentication status route

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/status", (req,res)=>{
+
+res.json({
+success:true,
+message:"Auth Route Working"
+});
+
+});
+
+module.exports = router;
